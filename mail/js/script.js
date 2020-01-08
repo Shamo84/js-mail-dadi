2215090c883fec5ff0cacd717ed9197b83471bbd
Andrea Pacifico <pacifico.mensa@gmail.com>
2020-01-08 11:29:31 +0100

    up

--- mail/js/script.js
+++ mail/js/script.js
@@ -1,17 +1,15 @@
 var ListaEmail = ['topolino@gmail.com', 'paperino@gmail.com', 'minni@gmail.com', 'pluto@gmail.com', 'paperone@gmail.com', 'quiquoqua@gmail.com', 'paperoga@gmail.com'];
-var trovato = false;
 
 var EmailUtente;
 EmailUtente = prompt('inserisci la tua email:');
+var messaggio = "non sei nella lista, non puoi entrare";
 
 for (var i = 0; i < ListaEmail.length; i++) {
+
   if (ListaEmail[i] == EmailUtente) {
-    trovato = true;
+    i = ListaEmail.length;
+    messaggio = "sei nella lista, puoi entrare";
   }
 }
 
-if (trovato  == true) {
-  alert('sei nella lista, puoi entrare.');
-} else {
-  alert('non sei nella lista, non puoi entrare');
-}
+  alert(messaggio);
